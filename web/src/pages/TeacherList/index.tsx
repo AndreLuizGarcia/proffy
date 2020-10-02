9ddd5eaf6f345f87784b4a44790ca40941e55050
Andre Luiz Garcia <andre.luiz@gec.inatel.br>
2020-10-02 14:58:37 -0300

    Add files via upload

---
+++ web/src/pages/TeacherList/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import Input from '../../components/Input'
+import PageHeader from '../../components/PageHeader'
+import TeacherItem from '../../components/TeacherItem'
+
+import './styles.css'
+
+function TeacherList() {
+  return (
+    <div id="page-teacher-list" className="container">
+      <PageHeader title="Estes são os proffys disponívies.">
+        <form id="search-teachers">
+          <Input name="subject" label="Matéria" />
+          <Input name="week_day" label="Dia da semana" />
+          <Input type="time" name="time" label="Hora" />
+        </form>
+      </PageHeader>
+
+      <main>
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+      </main>
+    </div>
+  )
+}
+
+export default TeacherList
